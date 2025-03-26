326b72b02f6a9e8f12aac55b976015282bec4e2c
arnau-romero-hazas <aromerohazas@gmail.com>
2025-03-26 11:42:58 +0100

    tryin to fix image in ghPages with path relative .env3

--- hangman/src/components/HangmanImage.jsx
+++ hangman/src/components/HangmanImage.jsx
@@ -1,9 +1,9 @@
 export default function HangmanImage({ attemptsLeft }) {
-  console.log("ATTEMPTS LEFT en HangmanImage:", attemptsLeft);
-  return (
+console.log("ATTEMPTS LEFT en HangmanImage:", attemptsLeft);
+const baseUrl = import.meta.env.VITE_BASE_URL;
+return (
       <img
-        src={`/hangman-${6 - attemptsLeft}.jpg`}
-        alt={`Hangman stage ${6 - attemptsLeft}`}
+        alt={` ${baseUrl}Hangman stage ${6 - attemptsLeft}`}
         className="hangman-image"
       />
     );
